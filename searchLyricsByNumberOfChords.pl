f027439bce2b8c08bbd8f1b6caa19e733a7de0b3
Fabiana Ferreira <fabiana.ferreira@poli.ufrj.br>
2019-05-20 21:58:33 -0300

    cleaning code

--- searchLyricsByNumberOfChords.pl
+++ searchLyricsByNumberOfChords.pl
@@ -7,17 +7,13 @@ sub SearchLyricsByNumberOfChords
 	my @list = ();
 	my $filename = $_[0];
     my $quantity = $_[1];
-    my $chordsLinesCounter = 0;
-    my $counter = 0;
 	open (my $fh, $filename) or die "Error: opening file '$filename'";
 
 	while (my $line = <$fh>)
 	{
-        $counter++;
         my $isChordLine = checkChordsLinesInFile($line);
 
         if ($isChordLine == 1) {
-            $chordsLinesCounter = $chordsLinesCounter + 1;
             while ($line =~ /([CDEFGAB]?\S+)/g)
             {
                 push (@list, ($1));       
@@ -29,10 +25,6 @@ sub SearchLyricsByNumberOfChords
 
     my @result = keys %{ { map { $_=> 1 } @list } };
     my $chordsQuantity = @result;
-
-    # print "DEBUG: Chords line quantity: $chordsLinesCounter \n";
-
-    # print "DEBUG: Chords quantity: $chordsQuantity \n";
 
     if ($quantity == $chordsQuantity) {
         return 1;
@@ -48,7 +40,7 @@ my $number = <STDIN>;
 
 chomp $number;
 
-my @lyrics = ("./cifras/eu_sei_que_vou_te_amar.txt", "./cifras/a_lenda.txt", "./cifras/era_uma_vez.txt", "./cifras/imortal.txt", "./cifras/as_quatro_estacoes.txt", "./cifras/sonho_azul.txt");
+my @lyrics = ("./cifras/era_uma_vez.txt", "./cifras/imortal.txt", "./cifras/as_quatro_estacoes.txt");
 
 foreach (@lyrics) {
     my $isAMatch = SearchLyricsByNumberOfChords($_, $number);
